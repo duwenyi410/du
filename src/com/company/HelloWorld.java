0c1f3461f7bbe62dd1f595af20fed51cb4555c9b
java1234 <java1234@qq.com>
2022-04-19 15:22:33 +0800

    5

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("duwenyi1");
+        System.out.println("duwenyi23");
+        System.out.println("duwenyi24");
+    }
+
+}
---
+++ src/java1234/com/TestClass.java
@@ -0,0 +1,11 @@
+package java1234.com;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("duwenyi6");
+
+	}
+
+}
